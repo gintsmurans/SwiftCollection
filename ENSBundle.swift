ede01a335b40ba9e99dc5d8623641a12cf096570
Gints Murāns <gm@gm.lv>
2015-01-04 02:52:22 +0200

    NSBundle pathForResource

---
+++ ENSBundle.swift
@@ -0,0 +1,23 @@
+//
+//  ENSBundle.swift
+//
+//  Created by Gints Murans on 17/12/14.
+//  Copyright (c) 2014 Gints Murans. All rights reserved.
+//
+
+import Foundation
+
+extension NSBundle {
+    func pathForResource(filename: String) -> String? {
+        if self.resourcePath == nil {
+            return nil
+        }
+
+        var path_to_file = "\(self.resourcePath!)/\(filename)"
+        if NSFileManager.defaultManager().fileExistsAtPath(path_to_file) {
+            return path_to_file
+        }
+
+        return nil
+    }
+}
