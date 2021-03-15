6e552edb4e18207920f89241f9319511228263a9
Gints Murans <gm@gm.lv>
2021-03-15 21:07:42 +0200

    Removed tv and watch support

--- Package.swift
+++ Package.swift
@@ -6,9 +6,7 @@ let package = Package(
     name: "GMSwiftCollection",
     platforms: [
         .macOS(.v10_11),
-        .iOS(.v9),
-        .tvOS(.v9),
-        .watchOS(.v2)
+        .iOS(.v9)
     ],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
